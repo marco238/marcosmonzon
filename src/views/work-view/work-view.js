feadb4a726ea96adbe672d28ba3765c4b565c81f
Marco <marco238cuba@gmail.com>
2019-11-04 13:04:27 +0100

    feat: generate projects cards fron array

--- src/views/work-view/work-view.js
+++ src/views/work-view/work-view.js
@@ -21,19 +21,43 @@ class WorkView  extends LitElement {
   }
 
   static get properties() {
-    return {};
+    return {
+      projects: {type: Array}
+    };
   }
 
   constructor() {
     super();
+    this.projects = [
+      {
+        title: 'Community',
+        description: 'Community is a digital product to improve neighborhood coexistence.',
+        backgroundImage: '../../../assets/images/community.png',
+        icons: [],
+        redirectUrl: 'https://ihcommunity.github.io/Community-web/',
+      }
+    ];
   }
 
   render() {
     return html`
       <div class="work-container">
-        <card-element></card-element>
+        ${this._generateProjects()}
       </div>
     `;
+  }
+
+  _generateProjects() {
+    return this.projects.map((project) => {
+      return html`
+        <card-element
+          title="${project.title}"
+          description="${project.description}"
+          backgroundImage="${project.backgroundImage}"
+          redirectUrl="${project.redirectUrl}">
+        </card-element>
+      `;
+    });
   }
 }
 
