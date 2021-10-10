ead677e546aac912774f497fe1a5ec7e806540ae
marco238 <marco238cuba@gmail.com>
2021-10-10 05:38:46 +0200

    Updates

--- view-404.bundle.js
+++ view-404.bundle.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{5:function(e,t,s){"use strict";s.r(t);var n=s(0),r=s(1);class c extends n.a{static get styles(){return[r.a,n.b`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{5:function(e,t,s){"use strict";s.r(t);var n=s(0),r=s(1);class i extends n.a{static get styles(){return[r.a,n.b`
         :host {
           display: flex;
           justify-content: center;
@@ -6,6 +6,13 @@
           height: 100vh;
           color: #ffffff;
         }
+
+        .not-found-image {
+          max-width: 500px;
+        }
       `]}static get properties(){return{}}constructor(){super()}render(){return n.c`
-      <h2>404 - Not found :(</h2>
-    `}}customElements.define("view-404",c)}}]);
+      <img
+        src="https://res.cloudinary.com/dlmrvaeyh/image/upload/v1633836982/404.png"
+        class="not-found-image"
+      />
+    `}}customElements.define("view-404",i)}}]);
